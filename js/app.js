b5be5b276ac08310729e59e21fdf2c66648652a0
northsal <northupst@gmail.com>
2014-11-28 15:54:38 -0800

    hard coded version 1-working

--- js/app.js
+++ js/app.js
@@ -1,77 +1,126 @@
-// List of Seattle Traffic Cameras
-// http://data.seattle.gov/resource/65fc-btcc.json
 
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
-
 $(document).ready(function() {
+    var map;
+    var geocoder;
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var addr1 = 'University of Washington, Seattle, WA';
+    var addr2 = "Seattle University, Seattle, WA";
+    var mode;
     var mapElem = document.getElementById('map');
     var center = {
         lat: 47.6,
         lng: -122.3
     };
-
-    var map = new google.maps.Map(mapElem, {
+    map = new google.maps.Map(mapElem, {
         center: center,
         zoom: 12
     });
-    //var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-    directionsDisplay.setMap(map);
-
-    //create a geocoder object
-    var geocoder = new google.maps.Geocoder();
-
-    //geocode an address
-    var addr1 = 'University of Washington, Seattle, WA';
-    var coords1;
-    geocoder.geocode({address: addr1}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            //results is an array of possible matches
-            //coordiantes for each are in geometery.location
-            var coords = results[0].geometry.location;
-            coords1 = new google.maps.LatLng(coords.lat(), coords.lng());
-            var marker = new google.maps.Marker({
-                position: coords1,
-                map: map
-            });
-        }
+    //We need to set start and end destinations, as well as travel mode.
+    //This should be done in the initialize function
+    $("#calculate").click(function() {
+        initialize();
+        placeMarkers();
+        calcRoute();
+        calculateDistances();
     });
 
-    var addr2 = "Seattle University, Seattle, WA";
-    var coords2;
-    geocoder.geocode({address: addr2}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            //results is an array of possible matches
-            //coordiantes for each are in geometery.location
-            var coords = results[0].geometry.location;
-            coords2 = new google.maps.LatLng(coords.lat(), coords.lng());
-            var marker = new google.maps.Marker({
-                position: coords2,
-                map: map
-            });
+    function initialize() {
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        var center = {
+            lat: 47.6,
+            lng: -122.3
+        };
+        map = new google.maps.Map(mapElem, {
+            center: center,
+            zoom: 12
+        });
+        geocoder = new google.maps.Geocoder();
+        directionsDisplay.setMap(map);
+        mode = "DRIVING";
+    }
+
+    function placeMarkers() {
+        geocoder.geocode({address: addr1}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var coords = results[0].geometry.location;
+                var marker = new google.maps.Marker({
+                    position: coords,
+                    map: map
+                });
+            }
+
+        });
+        geocoder.geocode({address: addr2}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var coords = results[0].geometry.location;
+                var marker = new google.maps.Marker({
+                    position: coords,
+                    map: map
+                });
+            }
+
+        });
+    }
+
+    function calcRoute() {
+        var request = {
+            origin:addr1,
+            destination: addr2,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function(response, status) {
+            if(status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+        });
+    }
+
+    function calculateDistances() {
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix ({
+            origins: [addr1],
+            destinations: [addr2],
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.IMPERIAL,
+            avoidHighways: false,
+            avoidTolls: false
+        }, callback);
+    }
+
+    function callback(response, status) {
+        if (status != google.maps.DistanceMatrixStatus.OK) {
+            alert('Error was: ' + status);
+        } else {
+            var origins = response.originAddresses;
+            var destinations = response.destinationAddresses;
+            var outputDiv = document.getElementById('outputDiv');
+            outputDiv.innerHTML = '';
+
+            for (var i = 0; i < origins.length; i++) {
+                var results = response.rows[i].elements;
+                for (var j = 0; j < results.length; j++) {
+                    outputDiv.innerHTML += origins[i] + ' to ' + destinations[j]
+                        + ': ' + results[j].distance.text + ' in '
+                        + results[j].duration.text + '<br>';
+                        calculateEmissions(results[j].distance.value);
+                }
+            }
         }
-    });
-    calcRoute(coords1, coords2);
+    }
+
+    function calculateEmissions(distance) {
+        var result = 0;
+        if(mode == "DRIVING") {
+            result = (0.96 * distance);
+        } else if(mode == "TRANSIT") {
+            result = (0.64 * distance);
+        }
+        var total = (result / 1609.344);
+        var value = total.toFixed(2);
+        var emissions = document.getElementById('emissions');
+        emissions.innerHTML = "Total emissions: " + value;
+    }
 });
-
-function calcRoute(start, end) {
-    var directionsService = new google.maps.DirectionsService();
-    var request = {
-        origin:start,
-        destination: end,
-        travelMode: google.maps.TravelMode.DRIVING
-    };
-    directionsService.route(request, function(response, status) {
-        if(status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-        }
-    });
-};
